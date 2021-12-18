1da2e04a96110e49907f0e4692082a4dd787b112
Ankush Dua <ankushduacodes@gmail.com>
2021-12-18 20:38:00 +0530

    added routes and files for the routes to be rendered

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-
-const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home,
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
-];
+import routes from './route';
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  linkActiveClass: 'active',
 });
 
 export default router;
