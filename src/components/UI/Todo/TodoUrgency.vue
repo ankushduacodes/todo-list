f60db39a3d206d312807ccb2ff185f4d7af60960
Ankush Dua <ankushduacodes@gmail.com>
2021-12-21 02:23:45 +0530

    added todo urgency component

---
+++ src/components/UI/Todo/TodoUrgency.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tag">
+    <p class="rounded-circle">
+      {{ (badgeColor.toLowerCase() === 'red') ? '🔴' : (badgeColor === 'green' ? '🟢' : '🟡') }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    badgeColor: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tag {
+  display: flex;
+  align-items: center;
+}
+.rounded-circle {
+  margin: 0 0 0 10px;
+}
+</style>
